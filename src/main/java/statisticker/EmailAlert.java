eeddad08d602bf2114a1d6898f5b24277e283665
praveen <pjmothkuri@gmail.com>
2022-10-12 10:19:10 +0530

    Test-1
    Test-1

---
+++ src/main/java/statisticker/EmailAlert.java
@@ -0,0 +1,17 @@
+/**
+ * <copyright>
+ *
+ * Copyright (c) 2022 ETAS GmbH. All rights reserved.
+ *
+ * </copyright>
+ */
+package statisticker;
+
+/**
+ *
+ */
+public class EmailAlert implements IAlerter {
+
+	boolean emailSent = false;
+
+}
---
+++ src/main/java/statisticker/LEDAlert.java
@@ -0,0 +1,17 @@
+/**
+ * <copyright>
+ *
+ * Copyright (c) 2022 ETAS GmbH. All rights reserved.
+ *
+ * </copyright>
+ */
+package statisticker;
+
+/**
+ *
+ */
+public class LEDAlert implements IAlerter {
+
+	boolean ledGlows = false;
+
+}
